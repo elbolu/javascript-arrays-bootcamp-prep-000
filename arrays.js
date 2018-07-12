ccffd69a53597db7bd2b254b30ea0bfb9bb4740a
Leah Hill <leah@verysandwich.com>
2018-07-12 15:24:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -11,7 +11,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array) {
-  element =  [array,..."foo"];
+  element = [array,..."foo"];
   return(element);
 }
 

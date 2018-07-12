64ad579bb246a5f132bc0941410777d1237914cc
Leah Hill <leah@verysandwich.com>
2018-07-12 15:13:43 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,14 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+const array = [1]
+function addElementToBeginningOfArray(array) {
+  element =  ["foo",...array];
+  return(element);
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element);  
+}
+
+
+  
